3e93e9219494a165e551b31e8a2ea48c98ba1563
Villasdaniel <2057@holbertonschool.com>
2020-08-20 16:15:54 -0500

    update holberton.h with structure description, update printf.c with betty style, update shell.c using standard strtok function and update function splitline function suing standard strtok function

--- printf.c
+++ printf.c
@@ -1,52 +1,5 @@
 #include "holberton.h"
 /**
-* _printf - print c,s,f,u,i,d
-* @format: arguments
-* Return: length of all characters
-*/
-int _printf(const char *format, ...)
-{
-form types[] = {
-	{"c", p_char}, {"s", p_str}, {"d", p_int}, {"i", p_int}, {NULL, NULL}};
-	va_list arguments;
-	int pos = 0, length = 0, pos_form;
-
-	va_start(arguments, format);
-	if (!format || !format[pos])
-		return (-1);
-	for (; format[pos]; pos++)
-	{
-		if (format[pos] == '%')
-		{
-			if (format[pos + 1] == '\0')
-				return (-1);
-			if (format[pos + 1] == '%')
-				_putchar(37), length++;
-			else
-			{
-				for (pos_form = 0; types[pos_form].ch != NULL; pos_form++)
-				{
-					if (types[pos_form].ch[0] == format[pos + 1])
-					{
-						length += types[pos_form].func(arguments);
-						break;
-					}
-				}
-				if (types[pos_form].ch == NULL)
-				{
-					_putchar(format[pos]);
-					_putchar(format[pos + 1]), length += 2;
-				}
-			}
-			pos++;
-		}
-		else
-			_putchar(format[pos]), length++;
-	}
-	va_end(arguments);
-	return  (length);
-}
-/**
 * _putchar - writes the character c to stdout
 * @c: The character to print
 * Return: On success 1.
@@ -122,4 +75,52 @@ int p_str(va_list arguments)
 	_putchar(p[a]);
 	}
 	return (a);
+}
+
+/**
+* _printf - print c,s,f,u,i,d
+* @format: arguments
+* Return: length of all characters
+*/
+int _printf(const char *format, ...)
+{
+form types[] = {
+	{"c", p_char}, {"s", p_str}, {"d", p_int}, {"i", p_int}, {NULL, NULL}};
+	va_list arguments;
+	int pos = 0, length = 0, pos_form;
+
+	va_start(arguments, format);
+	if (!format || !format[pos])
+		return (-1);
+	for (; format[pos]; pos++)
+	{
+		if (format[pos] == '%')
+		{
+			if (format[pos + 1] == '\0')
+				return (-1);
+			if (format[pos + 1] == '%')
+				_putchar(37), length++;
+			else
+			{
+				for (pos_form = 0; types[pos_form].ch != NULL; pos_form++)
+				{
+					if (types[pos_form].ch[0] == format[pos + 1])
+					{
+						length += types[pos_form].func(arguments);
+						break;
+					}
+				}
+				if (types[pos_form].ch == NULL)
+				{
+					_putchar(format[pos]);
+					_putchar(format[pos + 1]), length += 2;
+				}
+			}
+			pos++;
+		}
+		else
+			_putchar(format[pos]), length++;
+	}
+	va_end(arguments);
+	return  (length);
 }
